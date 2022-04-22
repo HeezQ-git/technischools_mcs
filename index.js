9b6c951ef3eb353940b35919926efbc30087c7af
HeezQ <94010785+WiktorPawlikowski@users.noreply.github.com>
2022-04-22 13:56:03 +0200

    build

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
@@ -8,7 +9,10 @@ const usersApi = require('./backend/backoffice/users');
 const groupsApi = require('./backend/backoffice/groups');
 const config = require('./backend/config/');
 
-app.use('/', express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(cookieParser());
